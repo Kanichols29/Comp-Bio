2b205fb7fe605732f23677a73f0483b2f61540b4
Kanichols29 <kanichols29@vt.edu>
2026-09-16 22:41:55 -0400

    Add Practical W4 scripts

---
+++ Practicals/W4/about_me.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Define variables that contain information about me
+name = "Kaitlyn"
+favorite_color = "Blue"
+favorite_activity = "Traveling"
+favorite_animal = "Dog"
+
+# Print my name and favorite things
+print("My name:", name)
+print("My favorite color:", favorite_color)
+print("My favorite activity:", favorite_activity)
+print("My favorite animal:", favorite_animal)
